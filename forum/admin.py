eb7e14b9bad0d848504e37a71428c60295fd85f0
Louis Barillon <louis85550@live.fr>
2022-04-01 10:19:24 +0200

    [Mdf] conflict admin account

--- forum/admin.py
+++ forum/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Tag, Answer
-from account.models import Profile
 
 # QuestionVote, #AnswerVote
 class QuestionAdmin(admin.ModelAdmin):
@@ -14,8 +13,6 @@ class TagAdmin(admin.ModelAdmin):
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ['answer', 'date_creation', 'id', 'question', 'profile', 'id']
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = [ 'user', 'point', 'bio', 'first_name', 'last_name', 'id']
 
 #class QuestionVoteAdmin(admin.ModelAdmin):
 #    list_display = ['question', 'profile', 'date_creation']
@@ -27,7 +24,6 @@ class ProfileAdmin(admin.ModelAdmin):
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Tag, TagAdmin )
 admin.site.register(Answer, AnswerAdmin )
-admin.site.register(Profile, ProfileAdmin )
 
 
 
