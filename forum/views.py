ff64ff10af72a4103d73014a2f750774513deab8
Louis Barillon <louis85550@live.fr>
2022-04-01 11:16:33 +0200

    [add] functionnal views, it stays Upgrade view

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
-from .forms import CreateQuestion, Details
+from .forms import CreateQuestion
 from django.views.generic import CreateView, ListView, UpdateView, DetailView
-from .models import Question, Answer
+from .models import QuestionModel, Answer
 from django.urls import reverse_lazy
 from django.contrib import messages
 
@@ -31,102 +31,37 @@ from django.contrib import messages
 
 
 # Create your views here.
-def Create(request):
+def create(request):
     html_template = "Question.html"
     context = {}
     form = CreateQuestion(request.POST or None)
     if form.is_valid():
         form.save()
-
         return HttpResponseRedirect("/")
-
     context['form'] = form
     return render(request, html_template, context)
 
-
-def List(request):
+def list(request):
     html_template = "Feed.html"
     context = {}
-    context['dataset'] = Question.objects.all()
-
+    context['dataset'] = QuestionModel.objects.all()
     return render(request, html_template, context)
 
 
-def Details(request, id):
+def details(request, pk):
     html_template = "Detail.html"
     context = {}
-    context['data'] = Question.objects.get(id=id)
-    form = Details(request.POST or None)
-    context['form'] = form
+    context['data'] = QuestionModel.objects.get(id=pk)
     return render(request, html_template, context)
 
 
-def Update(request, id):
-    html_template = "Update.html"
-    context = {}
-    obj = get_object_or_404(Question, id = id)
-    form = CreateQuestion(request.POST or None, instance=obj)
-
-    if form.is_valid():
-        form.save()
-        return HttpResponseRedirect("/" + id)
-
-    context['form'] = form
-
-    return render(request, html_template, context)
-
-class C_Question(CreateView):
-    model = Question
-    html_template = "Question.html"
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        context['form'] = CreateQuestion()
-        return render(request, self.html_template, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {}
-        form = CreateQuestion(request.POST or None)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/")
-        context['form'] = form
-        return render(request, self.html_template, context)
-
-class D_Question(DetailView):
-    model = Question
-    html_template = "Detail.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['data'] = Question.objects.get(id=id)
-        return context
-
-
-class L_Question(ListView):
-    model = Question
-    html_template = "Feed.html"
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        context['dataset'] = Question.objects.all()
-        return render(request, self.html_template, context)
-
-class U_Question(UpdateView) :
-    model = Question
-    html_template = "Update.html"
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        context['form'] = Update()
-        return render(request, self.html_template, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {}
-        form = Update(request.POST or None, id)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/')
-        context['form'] = form
-        return render(request, self.html_template, context)
-
+#def Update(request, pk):
+    #html_template = "Update.html"
+    #context = {}
+    #obj = get_object_or_404(QuestionModel, id = pk)
+    #form = CreateQuestion(request.POST or None, instance=obj)
+    #if form.is_valid():
+        #form.save()
+        #return HttpResponseRedirect("/" + id)
+    #context['form'] = form
+    #return render(request, html_template, context)
