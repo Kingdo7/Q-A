8437ac7ed1ed460d86dd2af60d7a0492af655705
Louis Barillon <louis85550@live.fr>
2022-04-01 10:22:10 +0200

    [Fix] views unused

--- forum/forms.py
+++ forum/forms.py
@@ -1,12 +1,8 @@
 from django.forms import ModelForm
-from .models import Question_model
+from .models import ModelQuestion
 
 class CreateQuestion(ModelForm):
-    class Meta :
-        model = Question_model
+    class Meta:
+        model = ModelQuestion
         fields = ['title', 'question']
 
-class Details(ModelForm):
-    class Meta :
-        model = Question_model
-        fields = ['answer']
