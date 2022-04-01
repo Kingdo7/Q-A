b107e2a6184009941d0740fd1f9eefecc096a363
Louis Barillon <louis85550@live.fr>
2022-04-01 10:23:50 +0200

    [Fix] path error with DetailView

--- forum/urls.py
+++ forum/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from . import views
-from .views import (
-    C_Question,
-    D_Question,
-    U_Question,
-    L_Question,
-)
+
 
 app_name = 'forum'
 
 
 urlpatterns = [
     path("create/", views.Create, name = "Question"),
-    #path("<slug:id>/update", views.Update, name="update"),
-    path("detail/<int:id>/", views.Details, name="details"),
+    #path("/<str:pk>/update", views.Update, name="update"),
+    path("detail/<str:pk>/", views.Detail, name="details"),
     path("", views.List, name='feed'),
 
 ]
