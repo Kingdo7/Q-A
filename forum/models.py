d303d22d852df8cd1dcbcbb5ea8f1c8e8eb76a91
Louis Barillon <louis85550@live.fr>
2022-04-01 10:22:29 +0200

    [Mdf] better model

--- forum/models.py
+++ forum/models.py
@@ -8,7 +8,6 @@ from django.utils.crypto import get_random_string
 from django.utils.text import slugify
 
 # Create your models here.
-from django.db.models import DateTimeField
 from account.models import Profile
 
 class Tag(models.Model):
@@ -66,7 +65,7 @@ class Question(models.Model):
 class Answer(models.Model):
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="questionanswer")
-    profile = models.OneToOneField(User, on_delete=models.CASCADE, related_name="useranswer")
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="useranswer")
     answer = models.CharField(max_length=2000)
     date_creation = models.DateTimeField(auto_now_add=True)
     votelist = models.ManyToManyField(Profile, blank=True,  related_name="voteanswerslist")
@@ -88,55 +87,18 @@ class Answer(models.Model):
             super(Answer, self).save(*args, **kwargs)
 
 
-class AnswerVote(models.Model):
-
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name="answervote")
-    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="useranswervote")
-    date_creation = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(null=True, blank=True)
-
-    def __str__(self):
-        return str(self.profile) or ""
-
-    def get_absolute_url(self):
-        return reverse('forum:answervote-detail',
-                       kwargs={'pk': self.pk})
-
-    def save(self, *args, **kwargs):
-        if self.slug:
-            super(AnswerVote, self).save(*args, **kwargs)
-        else:
-            self.slug = slugify(get_random_string(12))
-            super(AnswerVote, self).save(*args, **kwargs)
-
-
-class Question_model(models.Model):
-    title = models.CharField(max_length=100)
-    question = models.TextField()
-    answer = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+class ModelQuestion(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="Author")
-    tag = models.ManyToManyField(Tag)
-    slug = models.SlugField(null=True, blank=True)
-
+    title = models.CharField(max_length=50)
+    question = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    #tag = models.ManyToManyField(Tag)
     #visit_counter = models.PositiveIntegerField(default=0)
     #liked = models.ManyToManyField(User)
 
+    class Meta :
+        ordering = ['updated', 'created']
+
     def __str__(self):
-        return str(self.title) or ""
-
-
-    def get_absolute_url(self):
-        return reverse('forum:details',
-                       kwargs={'id': self.slug}
-                       )
-
-    def save(self, *args, **kwargs):
-        if self.slug:
-            super(Question_model, self).save(*args, **kwargs)
-        else:
-            self.slug = slugify(self.title + get_random_string(9))
-            super(Question_model, self).save(*args, **kwargs)
-
-
-
+        return self.title
